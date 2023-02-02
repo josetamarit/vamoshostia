cb94e322bd2ea82ed5d361949c3f1d9633a0bb59
josetamarit <>
2023-02-02 12:06:29 +0100

    lab 02/02

---
+++ ros2_ws/build/examples_topics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/usuario/Documentos/GitHub/vamoshostia/ros2_ws/src/examples/topics
+# Build directory: /home/usuario/Documentos/GitHub/vamoshostia/ros2_ws/build/examples_topics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
