974f90e6d3749b195439c52c30f7a7e39df6129e
josetamarit <>
2023-02-16 12:58:48 +0100

    Versión 3 16/02 se mueve 1 metro y funciona

--- tb3_ws/src/tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/tb3_ws/src/robot_trajectory/src/square.cpp
@@ -16,12 +16,14 @@ int main(int argc, char * argv[])
 int i=0, n=1000;
   while (rclcpp::ok() && (i<n)) {
     i++;
-    message.linear.x= 1.0;
-    message.angular.z=1.0;
+    message.linear.x= 0.1;
+    message.angular.z=0;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
+  message.linear.x=0;
+  publisher->publish(message);
   rclcpp::shutdown();
   return 0;
 }
