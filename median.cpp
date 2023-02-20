51789b1a4f680bbae95d5120e6b5b290a11735e4
josetamarit <josetamaritfernandez@gmail.com>
2023-02-20 12:08:20 +0100

    Median V0

---
+++ median.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv){
+    double m=0, s=0;
+    int n=0, element;
+    vector<int>elements;
+    cin>>element;
+    while(not cin.eof()){
+        elements.push_back(element);
+        n+=1;
+        cin>>element;
+    }
+
+    cout<<"Median: "<<m<<endl;
+    return 0;
+}
