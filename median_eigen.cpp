f56d9245be365fb65e7f87b2059137d381c89c73
josetamarit <josetamaritfernandez@gmail.com>
2023-02-27 18:51:19 +0100

    Resuelto

---
+++ median_eigen.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Eigen/Dense>
+using namespace std;
+using Eigen::VectorXd;
+
+int main(){
+    double m=0, input;
+    int n=0;
+    VectorXd elements(1);
+    cout<< "Dame un número (0 para acabar): "<<endl;
+    cin>>input;
+
+    while (input != 0) {
+        elements.conservativeResize(n+1);
+        elements(n) = input;
+        n++;
+        cout << "Dame un número (0 para acabar): " << endl;
+        cin >> input;
+    }
+
+    sort(elements.data(), elements.data() + elements.size());
+    int middle = elements.size()/2;
+    if (n % 2 == 0) { 
+        cout << "Par " << endl;
+        m = double(elements(middle-1) + elements(middle))/2.0;
+    } else {
+        cout << "Impar " << endl;
+        m = elements(middle);
+    }
+
+    cout << "Mediana: " << m << endl;
+    return 0;
+}
