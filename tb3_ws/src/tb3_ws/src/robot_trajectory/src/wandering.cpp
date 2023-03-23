0e728b93bc12874bba4ae26101b584a534ab7350
josetamarit <josetamaritfernandez@gmail.com>
2023-03-23 12:43:56 +0100

    V1 wandering

--- tb3_ws/src/tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -1,20 +1,25 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 using namespace std::chrono_literals;
+
+void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
+  std::cout<<msg<<std::endl;
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("wandering");
-  auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
-  std_msgs::msg::String message;
-  auto publish_count = 0;
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto subscriber= node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, topic_callback);
+  geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(500ms);
 
   while (rclcpp::ok()) {
-    message.data = "Hello, world! " + std::to_string(publish_count++);
+    message.linear.x=0.0;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
