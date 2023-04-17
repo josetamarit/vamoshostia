87eeb56e219a97dcb940df30d8c308e3182ad49c
josetamarit <113520207+josetamarit@users.noreply.github.com>
2023-04-17 19:03:06 +0200

    Wandering V5

--- tb3_ws/src/tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -1,34 +1,50 @@
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include <cmath> //M_PI
 #include <Eigen/Dense>
-
 #include <vector>
-
 using namespace std::chrono_literals;
 
 std::vector<float> vector;
 float min_val;
+bool stop=false;
 
 void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
    {
-
-	vector = msg->ranges;
-
-	// Mostrar rango
-
-	for ( int i = 9; i>0; i--) {
     std::cout << vector[i] << " ";
+    
+
+    if (vector[0] < 1){
+    	stop = true;
     }
-@@ -25,6 +28,12 @@ void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
 
     std::cout << std::endl;
-
-    // Mostrar mínimo
-    
-    Eigen::Map<Eigen::VectorXf> eigen_vector(vector.data(), vector.size());
-
     min_val = eigen_vector.minCoeff();
 
     std::cout << "Minimum value: " << min_val << std::endl;
-   }
+
+    std::cout << vector[0] << std::endl;
+
+
+  const sensor_msgs::msg::LaserScan::SharedPtr msg;
+
+  while (rclcpp::ok() && stop==false) // move forward 
+  {
+   	message.linear.x = 0.5;
+  	message.angular.z = 0;
+    publisher->publish(message);
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+
+  }
+
+  message.linear.x = 0;
+  publisher->publish(message);
+  rclcpp::spin_some(node);
+  loop_rate.sleep();
+
+  rclcpp::shutdown();
+  return 0;
+}
