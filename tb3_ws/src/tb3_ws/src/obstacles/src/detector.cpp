36ecedd7dabcd450d232c3b99858752e08588b22
josetamarit <josetamaritfernandez@gmail.com>
2023-04-20 13:14:23 +0200

    Revert "correcciones varias para robot físico"
    This reverts commit c26abf583eb6bafee6d6b7303f70c7240093395e.

--- tb3_ws/src/tb3_ws/src/obstacles/src/detector.cpp
+++ tb3_ws/src/tb3_ws/src/obstacles/src/detector.cpp
@@ -37,7 +37,7 @@ int main(int argc, char * argv[])
   publisher = node->create_publisher<example_interfaces::msg::Bool>("obstacle", 10);
   node->declare_parameter("obs_angle_min", -M_PI/8);
   node->declare_parameter("obs_angle_max", M_PI/8);
-  node->declare_parameter("obs_threshold", 0.36);
+  node->declare_parameter("obs_threshold", 1.0);
   obs_angle_min = node->get_parameter("obs_angle_min").get_parameter_value().get<double>();
   obs_angle_max = node->get_parameter("obs_angle_max").get_parameter_value().get<double>();
   obs_threshold = node->get_parameter("obs_threshold").get_parameter_value().get<double>();
