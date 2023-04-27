b8741cd613d2b7512d172964c5626945c87e4a52
josetamarit <josetamaritfernandez@gmail.com>
2023-04-27 13:22:43 +0200

    example services done

---
+++ ros2_ws/src/build/examples_sum/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/usuario/Documentos/GitHub/vamoshostia/ros2_ws/src/sum
+# Build directory: /home/usuario/Documentos/GitHub/vamoshostia/ros2_ws/src/build/examples_sum
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
