8d682caa90b1bbeba7691bcf68724696c59a44ed
josetamarit <113520207+josetamarit@users.noreply.github.com>
2023-05-10 00:52:13 +0200

    rings V1, la anterior es la 2

--- tb3_ws/src/tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/tb3_ws/src/olympic/src/rings.cpp
@@ -1,44 +1,22 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-
-
 using namespace std::chrono_literals;
-
-
 int main(int argc, char * argv[])
 {
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
-
-
- auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
- geometry_msgs::msg::Twist message;
+ auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
+ std_msgs::msg::String message;
  auto publish_count = 0;
  rclcpp::WallRate loop_rate(500ms);
-
-
- 
  while (rclcpp::ok()) {
-
-
-     for(int i=0; i< 14; i++){  
-           message.angular.z = 1.0;
-           message.linear.x = 1 * 1.0;
-           publisher->publish(message);
-           rclcpp::spin_some(node);
-           loop_rate.sleep();
-      }
-      message.angular.z = 0.0;
-      message.linear.x = 0.0;
-      publisher->publish(message);
-      rclcpp::spin_some(node);
-      loop_rate.sleep();
-      rclcpp::shutdown();
+   message.data = "Hello, world! " + std::to_string(publish_count++);
+   publisher->publish(message);
+   rclcpp::spin_some(node);
+   loop_rate.sleep();
  }
-
-
+ rclcpp::shutdown();
  return 0;
 }
 
