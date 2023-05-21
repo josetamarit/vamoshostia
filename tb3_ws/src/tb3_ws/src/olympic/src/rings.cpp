cd1b8bb0acddd5730f485226fc18fa1aecef9092
josetamarit <113520207+josetamarit@users.noreply.github.com>
2023-05-21 16:27:32 +0200

    action rings V7, V8

--- tb3_ws/src/tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/tb3_ws/src/olympic/src/rings.cpp
@@ -1,57 +1,52 @@
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include <cmath>
+#include <chrono>
+#include <vector>
+#include <cmath> //M_PI
 #include <memory>
 #include <cstdlib>
+#include "rclcpp/rclcpp.hpp"
 #include "turtlesim/srv/set_pen.hpp"
 #include "turtlesim/srv/teleport_absolute.hpp"
-#include <vector>
-
-
-
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
 using turtlesim::srv::SetPen;
 using turtlesim::srv::TeleportAbsolute;
 
 
-int main(int argc, char * argv[])
+// void add(std::shared_ptr<SetPen::Request> request)
+// {
+//   RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+//               "Incoming request\nr: %ld, g: %ld, b: %ld, width: %ld, off: %ld",
+//               request->r, request->g, request->b, request->width, request->off);
+// }
+
+int main(int argc, char* argv[])
 {
- rclcpp::init(argc, argv);
- auto node = rclcpp::Node::make_shared("rings");
- auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("rings");
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+  node->declare_parameter("radius", 1.0);
+  geometry_msgs::msg::Twist message;
+  rclcpp::WallRate loop_rate(10ms);
 
+  double radius = node->get_parameter("radius").get_parameter_value().get<double>();
 
- node->declare_parameter("radius_length", 1.0);
- geometry_msgs::msg::Twist message;
- rclcpp::WallRate loop_rate(500ms);
+  double time_for_circle = 2 * M_PI * radius / 1.0; // time to complete a circle
+  int num_iterations = static_cast<int>(time_for_circle / 0.01); // number of iterations to complete a circle
+  
 
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to set pen.");
 
- double radius = node->get_parameter("radius_length").get_parameter_value().get<double>();
-
-
- double circle= 2* M_PI;
-
-
- int iterations = static_cast<int>(circle/0.500);
-
-
- RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to set pen.");
-
-
+  //set_pen client
   auto client = node->create_client<SetPen>("/turtle1/set_pen");
-
-
+  
   auto request = std::make_shared<SetPen::Request>();
+  // set pen yellow
+  request->width = 5; 
 
-
-  request->width = 5;    
- 
-  std::vector<int> red = {0, 0, 255, 255, 0};
-  std::vector<int> green = {0, 0, 0, 255, 255};
-  std::vector<int> blue = {0, 255, 0, 0, 0};
-
+  std::vector<int> r = {0, 0, 255, 255, 0};
+  std::vector<int> g = {0, 0, 0, 255, 255};
+  std::vector<int> b = {0, 255, 0, 0, 0};   
 
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
@@ -61,34 +56,25 @@ int main(int argc, char * argv[])
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
   }
 
-
   auto result = client->async_send_request(request);
-
-
+  // Wait for the result
   if (rclcpp::spin_until_future_complete(node,result) == rclcpp::FutureReturnCode::SUCCESS)
   {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Todo bien");
   } else  {
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service SetPen");
   }
-
-
-
-
+  
+  // teleport
   auto client_tp = node->create_client<TeleportAbsolute>("/turtle1/teleport_absolute");
-
-
+  
   auto request_tp = std::make_shared<TeleportAbsolute::Request>();
+  // tp middle
+  request_tp->x = 5.5; 
+  request_tp->y = 5.5; 
 
-
-  request_tp->x = 5.5;
-  request_tp->y = 5.5;
-
-
-  std::vector<double> x = {5.5, (5.5-2*radius), (5.5+2*radius), (5.5-radius), (5.5+radius)};
-  std::vector<double> y = {5.5, 5.5, 5.5, (5.5-radius), (5.5-radius)};  
-     
-
+  std::vector<double> x = {5.5, ((5.5-2*radius)-(0.2*radius)), ((5.5+2*radius)+(0.2*radius)), (5.5-radius-(0.2*radius)), (5.5+radius)+(0.2*radius)};
+  std::vector<double> y = {5.5, 5.5, 5.5, (5.5-radius), (5.5-radius)};   
 
   while (!client_tp->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
@@ -98,10 +84,7 @@ int main(int argc, char * argv[])
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
   }
 
-
   auto result_tp = client_tp->async_send_request(request_tp);
-
-
   // Wait for the result
   if (rclcpp::spin_until_future_complete(node,result_tp) == rclcpp::FutureReturnCode::SUCCESS)
   {
@@ -111,49 +94,33 @@ int main(int argc, char * argv[])
   }  
 
 
-
-
-  
-
-
   for (int i=0; i<5; i++) {
     request->off = 1;
-
-
-    request->r = red[i];
-    request->g = green[i];
-    request->b = blue[i];
-    request_tp->x = x[i];
+    request->width= 5;
+    request->r = r[i]; 
+    request->g = g[i]; 
+    request->b = b[i];
+    request_tp->x = x[i]; 
     request_tp->y = y[i];
     result = client->async_send_request(request);
     result_tp = client_tp->async_send_request(request_tp);
 
-
     request->off = 0;
-    result = client->async_send_request(request);
+    result = client->async_send_request(request); 
 
-	}
-
-
-  for (int i = 0; i < iterations; i++) {
-    message.linear.x = 1;
-    message.angular.z = 2*M_PI / circle;
+    for (int j = 0; j < num_iterations; j++) { // do circle
+      message.linear.x = 1;
+      message.angular.z = 2 * M_PI / time_for_circle;
+      publisher->publish(message);
+      rclcpp::spin_some(node);
+      loop_rate.sleep();
+    }
+    // send zero velocity to topic
+    message.linear.x = 0;
+    message.angular.z = 0;
     publisher->publish(message);
- 
-
-
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
- }
-
-
-  message.linear.x = 0;
-  message.angular.z = 0;
-  publisher->publish(message);
-
-
- rclcpp::shutdown();
- return 0;
+  }
+  //off
+  rclcpp::shutdown();
+  return 0;
 }
-
-
--- tb3_ws/src/tb3_ws/src/olympic/src/rings_action_server.cpp
+++ tb3_ws/src/tb3_ws/src/olympic/src/rings_action_server.cpp
@@ -8,23 +8,28 @@
 #include "turtlesim/srv/set_pen.hpp"
 #include "turtlesim/srv/teleport_absolute.hpp"
 
-
-
-
+using namespace std::chrono_literals;
+using turtlesim::srv::SetPen;
+using turtlesim::srv::TeleportAbsolute;
 using Rings = 
   olympic_interfaces::action::Rings;
+
 using GoalHandleRings = 
   rclcpp_action::ServerGoalHandle<Rings>;
+
+rclcpp::Node::SharedPtr node = nullptr;
+
 rclcpp_action::GoalResponse handle_goal(
   const rclcpp_action::GoalUUID & uuid, 
   std::shared_ptr<const Rings::Goal> goal)
 {
-  RCLCPP_INFO(rclcpp::get_logger("server"),
-  "Got goal request with radius %.2f", goal->radius);
+  RCLCPP_INFO(rclcpp::get_logger("server"), 
+    "Got goal request with radius %.2f", goal->radius);
   (void)uuid;
   return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 }
 
+// Callback for dealing with cancellation. This implementation just tells the client it accepted the cancellation
 rclcpp_action::CancelResponse handle_cancel(
   const std::shared_ptr<GoalHandleRings> goal_handle)
 {
@@ -33,6 +38,9 @@ rclcpp_action::CancelResponse handle_cancel(
   (void)goal_handle;
   return rclcpp_action::CancelResponse::ACCEPT;
 }
+
+
+// This last set of callbacks accepts a new goal and starts processing it.
 void execute(const std::shared_ptr<GoalHandleRings>);
 
 void handle_accepted(const std::shared_ptr<GoalHandleRings> goal_handle){
@@ -41,29 +49,109 @@ void handle_accepted(const std::shared_ptr<GoalHandleRings> goal_handle){
 
 
 void execute(const std::shared_ptr<GoalHandleRings> goal_handle) {
+  // Action related
   RCLCPP_INFO(rclcpp::get_logger("server"), 
     "Executing goal");
-  rclcpp::Rate loop_rate(1);
   const auto goal = goal_handle->get_goal();
   auto feedback = std::make_shared<Rings::Feedback>();
   auto result = std::make_shared<Rings::Result>();
   auto & ring_number = feedback->drawing_ring; // Reference to drawing_ring(ptr)
   auto & ring_angle = feedback->ring_angle; // Reference to ring_angle(ptr)
-    float radius = goal->radius;
+  
+  // Ring related
+  float radius = goal->radius;
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+  geometry_msgs::msg::Twist message;
 
+  double time_for_circle = 2 * M_PI * radius / 1.0; // time to complete a circle
+  int num_iterations = static_cast<int>(time_for_circle / 0.01); // number of iterations to complete a circle
+  rclcpp::WallRate loop_rate(10ms);
 
+  // iteration values
+  std::vector<int> r = {0, 0, 255, 255, 0};
+  std::vector<int> g = {0, 0, 0, 255, 255};
+  std::vector<int> b = {0, 255, 0, 0, 0};   
+
+  std::vector<double> x = {5.5, ((5.5-2*radius)-(0.2*radius)), ((5.5+2*radius)+(0.2*radius)), (5.5-radius-(0.2*radius)), (5.5+radius)+(0.2*radius)};
+  std::vector<double> y = {5.5, 5.5, 5.5, (5.5-radius), (5.5-radius)};   
+ 
+  //set_pen client
+  rclcpp::Client<SetPen>::SharedPtr client =
+	node->create_client<SetPen>("/turtle1/set_pen");
+	auto request = std::make_shared<SetPen::Request>();
+ 
+  // Initialize TeleportAbsolute service
+	rclcpp::Client<TeleportAbsolute>::SharedPtr client_tp =
+	node->create_client<TeleportAbsolute>("/turtle1/teleport_absolute");
+	auto request_tp= std::make_shared<TeleportAbsolute::Request>();
+   // tp middle
+  request_tp->x = 5.5; 
+  request_tp->y = 5.5;
+
+  auto result_tp = client_tp->async_send_request(request_tp);
+ 
+  for (int i=0; i<5; i++) {
+    ring_number = i+1;
+    ring_angle = 0;
+    request->off = 1;
+    request->width= 5;
+    request->r = r[i]; 
+    request->g = g[i]; 
+    request->b = b[i];
+    request_tp->x = x[i]; 
+    request_tp->y = y[i];
+    auto result = client->async_send_request(request);
+    result_tp = client_tp->async_send_request(request_tp);
+
+    request->off = 0;
+    result = client->async_send_request(request); 
+
+    for (int j = 0; j < num_iterations; j++) { // do circle
+      message.linear.x = 1;
+      message.angular.z = 2 * M_PI / time_for_circle;
+      publisher->publish(message);
+      loop_rate.sleep();
+
+      // Feedback
+      ring_number = i+1;
+			goal_handle->publish_feedback(feedback);
+    	RCLCPP_INFO(rclcpp::get_logger("server"), "Publish Feedback");
+      if (j == num_iterations/4) {
+        ring_angle = 90;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+      }else if (j == num_iterations/2) {
+        ring_angle = 180;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+      }else if (j == 3*num_iterations/4) {
+        ring_angle = 270;
+				goal_handle->publish_feedback(feedback);
+    		RCLCPP_INFO(rclcpp::get_logger("server"), 
+       		"Publish Feedback");
+      } 
+    }
+    // send zero velocity to topic
+    message.linear.x = 0;
+    message.angular.z = 0;
+    publisher->publish(message);
+    loop_rate.sleep();
+  }
 
     if (rclcpp::ok()) { // When completed, if ROS still running:
     result->rings_completed = ring_number;
     goal_handle->succeed(result);
     RCLCPP_INFO(rclcpp::get_logger("server"), 
       "Goal Succeeded");
+    rclcpp::shutdown();
   }
 }
 
 int main(int argc, char ** argv){
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("action_server");
+  node = rclcpp::Node::make_shared("action_server");
   auto action_server = 
     rclcpp_action::create_server<Rings>(node,
       "rings",
